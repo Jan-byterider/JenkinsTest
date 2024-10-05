13861f7917b2426de789b7c1705ccdbcd1f57b40
jan janssens <jan@byterider.be>
2024-10-05 05:22:12 +0200

    d

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,7 +16,7 @@ pipeline {
                 script {
                     println('Hello, World')
                     String scriptlocation = 'resources\\test.ps1'
-                    sh "powershell pwd"
+                    sh "powershell scriptlocation"
 
                 }
             }
