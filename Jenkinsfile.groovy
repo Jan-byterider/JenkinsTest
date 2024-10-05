e9ab14d4f37d35ede7dbfd01934083226aaa324f
jan janssens <jan@byterider.be>
2024-10-05 06:02:12 +0200

    d

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,26 +6,28 @@ def PowerShell(psCmd) {
 pipeline {
     agent any
     stages {
-        stage("Clone Git Repository") {
-            steps {
-                git(
-                    url: "https://github.com/Jan-byterider/JenkinsTest.git",
-                    branch: "main",
-                    changelog: true,
-                    poll: true
-                )
-            }
-        }        
-        stage('Hello World') {
-            steps {
-                script {
-                    println('Hello, World')
-                    String scriptlocation = '..\\resources\\test.ps1'
-                    PowerShell('pwd')
-                    PowerShell(scriptlocation)
+        
+            stage("Clone Git Repository") {
+                steps {
+                    git(
+                        url: "https://github.com/Jan-byterider/JenkinsTest.git",
+                        branch: "main",
+                        changelog: true,
+                        poll: true
+                    )
+                }
+            }        
+            stage('Hello World') {
+                steps {
+                    script {
+                        println('Hello, World')
+                        String scriptlocation = '..\\resources\\test.ps1'
+                        PowerShell('pwd')
+                        PowerShell('ls')
 
+                    }
                 }
             }
         }
     }
-}
+
