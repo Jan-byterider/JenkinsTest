9a3ad155ec8c04e5602378f7c1a7b0c32054619d
jan janssens <jan@byterider.be>
2024-10-05 06:38:21 +0200

    dd

--- resources/test.ps1
+++ resources/test.ps1
@@ -4,11 +4,13 @@ try{
     $dirs = Get-ChildItem C:\TEMP -Directory 
     write-host "cleaning c:temp containing $items"
     if($items){
+        write-host "files to delete: $files"
         $files | Remove-Item -Force -Confirm:$false
     }else{
         write-host 'no files found - no delete action on files'
     }
     if($dirs){
+        write-host "dirs to delete: $dirs"
         $dirs | Remove-Item -Force -Confirm:$false -Recurse
     }else{
         write-host 'no dirs found - no delete action on folders'
