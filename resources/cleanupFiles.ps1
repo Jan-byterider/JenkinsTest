3ea4cdf23b6add07414c0ffb0821802eb22cb7aa
jan <jan@byterider.be>
2024-10-07 00:49:08 +0200

    deb

--- resources/cleanupFiles.ps1
+++ resources/cleanupFiles.ps1
@@ -32,63 +32,80 @@ write-host "password: $secret"
 
 $today = Get-Date
 $tresholdDate = $today.AddDays(-10)
-try{
-    # set path to downloads folder
-    #$path = (New-Object -ComObject Shell.Application).Namespace('shell:Downloads').Self.Path
-    $path = $pathToClean
-    $path = (New-Object -ComObject Shell.Application).Namespace("shell:$path").Self.Path
-    $user = whoami.exe
-    write-host $user
-    
-    if ($createFiles -eq $true) {
-        for($i=0;$i -lt 10; $i++)
+$scriptblock = {
+    try{
+        # set path to downloads folder
+        #$path = (New-Object -ComObject Shell.Application).Namespace('shell:Downloads').Self.Path
+        try{
+            $path = $pathToClean
+            $check = Test-Path -path $path
+        }catch{
+            throw $error[0]
+        }
+        if(!$check)
         {
-            Set-Location $path
-            mkdir "dir$i" -Force
-            $newPath = join-path -path $path -childPAth "dir$i"
-            Set-Location $newPath
-            for($j=0;$j -lt 5; $j++){
-                mkdir "dir$j" -Force
-                $lastPath = Join-Path -Path $newPath -ChildPath "dir$j"
-                Set-Location $lastPath
-                for($g=0;$g -lt 7;$g++){
-                    New-Item -name "file$g.txt" -ItemType File -Value "Dit is file$g" -Force
-                }
-                set-location $newPath
-            }
-    
-        }    
-    }
-    $files = Get-ChildItem $path -File -Recurse
-    $dirs = Get-ChildItem $path -Directory 
-    write-host "--------------------------------------------------------"
-    write-host "cleaning $path containing $($files.count) files"
-    write-host "cleaning $path containing: "  (Get-ChildItem $pathToClean -Directory -recurse).count  " folders to delete"
-    write-host "--------------------------------------------------------"
-    if($files){
-        write-host "files to delete: $files"
-        if(!$dryRun){
-            foreach ($file in $files){
-                if ($file.LastWriteTime -le $tresholdDate) {
-                    Remove-Item $file -Force -Confirm:$false
-                }
-            }
+            throw "ERROR: $path does not exists on $hostname"
         }
-    }else{
-        write-host 'no files found - no delete action on files'
-    }
-    if($dirs){
-        write-host "dirs to delete: $dirs"
-        if(!$dryRun){
-            foreach($dir in $dirs){
-                if((Get-ChildItem $dir).count -eq 0){
-                    Remove-Item $dir -Force
+        #$path = (New-Object -ComObject Shell.Application).Namespace("shell:$path").Self.Path
+        $user = whoami.exe
+        write-host $user
+        
+        if ($createFiles -eq $true) {
+            for($i=0;$i -lt 10; $i++)
+            {
+                Set-Location $path
+                mkdir "dir$i" -Force
+                $newPath = join-path -path $path -childPAth "dir$i"
+                Set-Location $newPath
+                for($j=0;$j -lt 5; $j++){
+                    mkdir "dir$j" -Force
+                    $lastPath = Join-Path -Path $newPath -ChildPath "dir$j"
+                    Set-Location $lastPath
+                    for($g=0;$g -lt 7;$g++){
+                        New-Item -name "file$g.txt" -ItemType File -Value "Dit is file$g" -Force
+                    }
+                    set-location $newPath
+                }
+        
+            }    
+        }
+        $files = Get-ChildItem $path -File -Recurse
+        $dirs = Get-ChildItem $path -Directory 
+        write-host "--------------------------------------------------------"
+        write-host "cleaning $path containing $($files.count) files"
+        write-host "cleaning $path containing: "  (Get-ChildItem $pathToClean -Directory -recurse).count  " folders to delete"
+        write-host "--------------------------------------------------------"
+        if($files){
+            write-host "files to delete: $files"
+            if(!$dryRun){
+                foreach ($file in $files){
+                    if ($file.LastWriteTime -le $tresholdDate) {
+                        Remove-Item $file -Force -Confirm:$false
+                    }
                 }
             }
+        }else{
+            write-host 'no files found - no delete action on files'
         }
-    }else{
-        write-host 'no dirs found - no delete action on folders'
+        if($dirs){
+            write-host "dirs to delete: $dirs"
+            if(!$dryRun){
+                foreach($dir in $dirs){
+                    if((Get-ChildItem $dir).count -eq 0){
+                        Remove-Item $dir -Force
+                    }
+                }
+            }
+        }else{
+            write-host 'no dirs found - no delete action on folders'
+        }
+    }catch{
+        write-host "Error cleaning $path"
+        write-host $error[0]
     }
+}
+try{
+    Invoke-Command -Session $session -ScriptBlock $scriptblock -ErrorAction Stop
 }catch{
     write-host "Error cleaning $path"
     write-host $error[0]
