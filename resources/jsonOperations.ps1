990285079ab45dc356c961dd13112f0f2459aeb2
jan <jan@byterider.be>
2024-10-21 01:00:18 +0200

    b

--- resources/jsonOperations.ps1
+++ resources/jsonOperations.ps1
@@ -58,7 +58,7 @@ try {
     Get-Location
     $items = Get-ChildItem -Recurse 
     write-host $items
-    #Invoke-Command -Credential $cred -command { $using:shareJson | out-file -path resources\nasCleanupByRetentionDate.json -Force }
+    Invoke-Command -Credential $cred -command { $using:shareJson | out-file -path resources\nasCleanupByRetentionDate.json -Force }
     #write-host 
     
 }
