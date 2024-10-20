f7878f4daa6dc141cd27f2f45522114da3577c25
jan <jan@byterider.be>
2024-10-20 23:32:37 +0200

    added error handling

--- resources/jsonOperations.ps1
+++ resources/jsonOperations.ps1
@@ -14,19 +14,39 @@ param (
 write-host "Powershell script started - SharePath: $sharePath retentionDays: $retentionDays jsonFilePath: $jsonFilePath"
 
 #[boolean]$dryRun = $true#read json
-$jsonFileContent = Get-Content -Raw $jsonFilePath
-$jsonObj = [System.Collections.ArrayList]::new()
-[System.Collections.ArrayList]$jsonObj = ConvertFrom-Json $jsonFileContent
+try {
+    $jsonFileContent = Get-Content -Raw $jsonFilePath
+    $jsonObj = [System.Collections.ArrayList]::new()
+    [System.Collections.ArrayList]$jsonObj = ConvertFrom-Json $jsonFileContent
+} catch {
+    Write-Host "Error loading json $jsonFilePath"
+}
 
-$newJsonObject = New-Object -TypeName PSObject -Property @{
-    Share= $sharePath
-    Retention = [int32]$retentionDays
+try {
+    $newJsonObject = New-Object -TypeName PSObject -Property @{
+        Share= $sharePath
+        Retention = [int32]$retentionDays
+    }
+}
+catch {
+    Write-Host "Error creating PS object conaining share data"
 }
 
 $newJsonObject
-$jsonObj.Add($newJsonObject)
 
-$shareJson = $jsonObj | ConvertTo-Json 
-$shareJson | out-file -path $jsonFilePath -Force
-write-host 
+try {
+    $jsonObj.Add($newJsonObject)
+}
+catch {
+    Write-Host "Error adding share to JSON object array list"
+}
 
+try {
+    $shareJson = $jsonObj | ConvertTo-Json 
+    $shareJson | out-file -path $jsonFilePath -Force
+    write-host 
+    
+}
+catch {
+    Write-Host "Error writing json $jsonFilePath"
+}
