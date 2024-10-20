a904a3da511622bba40173eeb2111b1f83899c2b
jan <jan@byterider.be>
2024-10-20 16:20:20 +0200

    dev

---
+++ createGitBranch.groovy
@@ -0,0 +1,29 @@
+//@Library('sharedLibs@main')
+def powerShell(psCmd) {
+    psCmd=psCmd.replaceAll("%", "%%")
+    bat "powershell.exe -NonInteractive -ExecutionPolicy Bypass -Command \"\$ErrorActionPreference='Stop';[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;$psCmd;EXIT \$global:LastExitCode\""
+}
+
+/*parameters { 
+    string(name: 'hostname', defaultValue: '', description: 'target hostname')
+    boolean(name: 'dryRun', defaultValue: true , description: 'actions are not executed when false')
+    }*/
+
+pipeline {
+    agent any
+    options {
+        timestamps()
+    }
+    stages {
+        stage('Git Checkout'){
+            gitCheckout(
+                branch: "main",
+                url: "https://github.com/Jan-byterider/PSBackup.git"
+            )
+            dir('newBranch'){
+                checkout -b scm
+            }
+
+        }
+    }
+}
