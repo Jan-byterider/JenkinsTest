9db40b727bec92dee21f5ab471e380174cedd5e8
jan <jan@byterider.be>
2024-10-21 12:07:13 +0200

    d

--- addShareToJson.groovy
+++ addShareToJson.groovy
@@ -82,7 +82,7 @@ pipeline {
                         //bat "git switch -c origin/newJsonFileBranch"
                         sshagent(['sshGitKey']){
                             //bats "ssh git branch"
-                            sh "ssh git push -u origin newJsonFileBranch"
+                            bat ("git push -u origin newJsonFileBranch")
                         }
                         
                         bat "git merge origin newJsonFileBranch"
