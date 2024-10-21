7a7b45ae3deef29586cd0b3b4cc9b84d3c6daff4
jan <jan@byterider.be>
2024-10-21 11:30:32 +0200

    dd

--- addShareToJson.groovy
+++ addShareToJson.groovy
@@ -39,7 +39,7 @@ pipeline {
         stage("Clone Git Repository") {
             steps {
                 git(
-                    url: "git@github.com:Jan-byterider/JenkinsTest.git",
+                    url: "https://github.com/Jan-byterider/JenkinsTest.git",
                     credentialsId: '77ec298e-4de3-4d77-bd17-73477c13ca95',
                     branch: "develop",
                     changelog: true,
