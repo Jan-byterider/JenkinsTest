fba19b0626883836b43cfb146c955473c4a2f5da
jan <jan@byterider.be>
2024-10-22 11:48:02 +0200

    d

--- addShareToJson.groovy
+++ addShareToJson.groovy
@@ -90,7 +90,7 @@ pipeline {
                         bat "git checkout upstream/develop"
                         try{
                             bat "git switch -c temp"
-                        } catch (exception e){
+                        } catch (err3){
                              println "Branch: temp already exists"
                         }
                         bat "git checkout -p temp"
